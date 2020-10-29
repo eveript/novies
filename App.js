f045ff2f2bb300006331df27bb14e33d2b058fe4
jinha <gojinha@naver.com>
2020-10-30 00:36:10 +0900

    cacheAsset

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import * as Font from "expo-font";
 
+import { AppLoading } from 'expo'
+import {Asset} from "expo-asset";
+import {Ionicons} from "@expo/vector-icons";
+
+
+const cacheImages = images => images.map(image => {
+    if (typeof image === 'string') {
+        return Image.prefetch(image)
+    } else {
+        return Asset.fromModule(image).downloadAsync()
+    }
+})
+
+const cacheFonts = fonts => fonts.map(font => Font.loadAsync(font))
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [isReady, setIsReady] = useState(false)
+    const loadAssets = () => {
+        const images = cacheImages([
+            "https://lh3.googleusercontent.com/ogw/ADGmqu8OM1cBYkzZ6Z4DVidC2d23yaGMRjrG-Ymn4K3g=s64-c-mo",
+            require("./assets/splash.png")
+        ])
+        console.log(images)
+        const fonts = cacheFonts([Ionicons.font])
+        return Promise.all([...images, ...fonts])
+    }
+    const onFinish = () => setIsReady(true)
+    return isReady ? <Text>Ready...</Text> : <AppLoading startAsync={loadAssets} onFinish={onFinish} onError={console.error} />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
