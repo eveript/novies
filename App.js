400390c3ee8345d95155b2869f2069b0e931b2dd
jinha <gojinha@naver.com>
2020-10-30 00:56:44 +0900

    NavigationContainer, Stack Navigator 추가

--- App.js
+++ App.js
@@ -1,33 +1,42 @@
 import { StatusBar } from 'expo-status-bar'
 import React, { useState } from 'react'
 import { StyleSheet, Text, View, Image } from 'react-native'
-import * as Font from "expo-font";
+import { NavigationContainer } from '@react-navigation/native'
+
+import * as Font from 'expo-font'
 
 import { AppLoading } from 'expo'
-import {Asset} from "expo-asset";
-import {Ionicons} from "@expo/vector-icons";
+import { Asset } from 'expo-asset'
+import { Ionicons } from '@expo/vector-icons'
+import Stack from './navigation/Stack'
 
+const cacheImages = (images) =>
+    images.map((image) => {
+        if (typeof image === 'string') {
+            return Image.prefetch(image)
+        } else {
+            return Asset.fromModule(image).downloadAsync()
+        }
+    })
 
-const cacheImages = images => images.map(image => {
-    if (typeof image === 'string') {
-        return Image.prefetch(image)
-    } else {
-        return Asset.fromModule(image).downloadAsync()
-    }
-})
-
-const cacheFonts = fonts => fonts.map(font => Font.loadAsync(font))
+const cacheFonts = (fonts) => fonts.map((font) => Font.loadAsync(font))
 export default function App() {
     const [isReady, setIsReady] = useState(false)
     const loadAssets = () => {
         const images = cacheImages([
-            "https://lh3.googleusercontent.com/ogw/ADGmqu8OM1cBYkzZ6Z4DVidC2d23yaGMRjrG-Ymn4K3g=s64-c-mo",
-            require("./assets/splash.png")
+            'https://lh3.googleusercontent.com/ogw/ADGmqu8OM1cBYkzZ6Z4DVidC2d23yaGMRjrG-Ymn4K3g=s64-c-mo',
+            require('./assets/splash.png'),
         ])
         console.log(images)
         const fonts = cacheFonts([Ionicons.font])
         return Promise.all([...images, ...fonts])
     }
     const onFinish = () => setIsReady(true)
-    return isReady ? <Text>Ready...</Text> : <AppLoading startAsync={loadAssets} onFinish={onFinish} onError={console.error} />
+    return isReady ? (
+        <NavigationContainer>
+            <Stack />
+        </NavigationContainer>
+    ) : (
+        <AppLoading startAsync={loadAssets} onFinish={onFinish} onError={console.error} />
+    )
 }
---
+++ screens/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { View, Text, Button } from 'react-native'
+
+export default ({ navigation }) => (
+    <View>
+        <Text>Home</Text>
+        <Button title="Go to Detail" onPress={() => navigation.navigate('Detail')} />
+    </View>
+)
